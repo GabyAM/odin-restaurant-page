3b7f9940378307c095d6eb64e0787d1dffc96868
GabyAM <gdgabyam@gmail.com>
2023-11-08 12:49:55 -0300

    adds more webpack dependencies.
    Added style-loader and css-loader to handle the css files on the webpack dist version. Also webpack support to fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: "production",
   module: {
     rules: [
       {
@@ -15,6 +16,10 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
