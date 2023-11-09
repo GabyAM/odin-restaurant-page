5051ed9ad3546d3a487f08d1faccc8ebbf3543f0
GabyAM <gdgabyam@gmail.com>
2023-11-08 23:12:16 -0300

    Revamps menu
    creates elements to display three menu items on the menu tab

--- src/loadMenu.js
+++ src/loadMenu.js
@@ -1,5 +1,36 @@
+import krabbyPatty from './images/krabbypatty.png'
+import kelpFries from './images/kelpfries.png'
+import seafoamSoda from './images/seafoamsoda.jfif'
+
+function createMenuItem(title, description, imageSource) {
+    const $container = document.createElement('div');
+
+    const $itemTitle = document.createElement('h3');
+    $itemTitle.textContent = title;
+
+    const $itemImage = document.createElement('img');
+    $itemImage.src = imageSource;
+
+    const $itemDescription = document.createElement('p');
+    $itemDescription.textContent = description;
+
+    $container.appendChild($itemTitle);
+    $container.appendChild($itemImage);
+    $container.appendChild($itemDescription);
+    return $container;
+}
+
 export function loadMenu() {
-    const $menu = document.createElement('p');
-    $menu.textContent = "Menu Highlights: 🍔 Krabby Patty: The classic and mouthwatering delight that put the Krusty Krab on the map.🍟 Krusty Fries: Crispy, golden perfection that pairs perfectly with our patties.🌊 Seafoam Soda: A fizzy, refreshing beverage straight from the ocean's depths.🍔🍟🍔Join us at the Krusty Krab and make every meal an adventure! Whether you're celebrating with friends, bringing the family, or just seeking a taste of Bikini Bottom's finest, the Krusty Krab is the place to be. Dive into a world of flavor, fun, and fantastic memories.Don't miss out on the Krusty Krab experience – come visit us today!"
-    document.querySelector('#content').appendChild($menu);
+    const $title = document.createElement('h1');
+    $title.textContent = 'Menu Highlights'
+    
+    const $krabbyPatty = createMenuItem('Krabby Patty', 'The classic and mouthwatering delight that put the Krusty Krab on the map.', krabbyPatty);
+    const $kelpFries = createMenuItem('Kelp Fries', 'Crispy, golden perfection that pairs perfectly with our patties.', kelpFries);
+    const $seafoamSoda = createMenuItem('Seafoam Soda', "A fizzy, refreshing beverage straight from the ocean's depths.", seafoamSoda);
+    
+    const $content = document.querySelector('#content');
+    $content.appendChild($title);
+    $content.appendChild($krabbyPatty);
+    $content.appendChild($kelpFries);
+    $content.appendChild($seafoamSoda);
 }
